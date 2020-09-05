442973ae71a3204d0dbf4d3c81124e09e8bbd05c
bradchao <brad@brad.tw>
2020-09-05 13:34:42 +0800

    2020090506

--- app/src/main/java/tw/org/tcca/apps/ftest2/F1.java
+++ app/src/main/java/tw/org/tcca/apps/ftest2/F1.java
@@ -7,12 +7,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
 
 public class F1 extends Fragment {
+    private View mainView;
+    private ViewFlipper viewFlipper;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f1, container, false);
+        mainView = inflater.inflate(R.layout.fragment_f1, container, false);
+        viewFlipper = mainView.findViewById(R.id.viewFlipper);
+        View f1 = viewFlipper.getChildAt(0);
+        View f2 = viewFlipper.getChildAt(1);
+        View f3 = viewFlipper.getChildAt(2);
+        View f4 = viewFlipper.getChildAt(3);
+
+
+        return mainView;
     }
 }
