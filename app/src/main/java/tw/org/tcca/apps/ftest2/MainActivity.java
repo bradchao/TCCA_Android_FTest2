b7e7d56092e85d6deb67baa56dfcf0ea86d92741
bradchao <brad@brad.tw>
2020-09-05 13:30:35 +0800

    2020090505

--- app/src/main/java/tw/org/tcca/apps/ftest2/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/ftest2/MainActivity.java
@@ -12,10 +12,12 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private ViewPager viewPager;
+    private F0 f0;
     private F1 f1;
     private F2 f2;
     private F3 f3;
-    private Fragment[] fs = new Fragment[3];
+    private F4 f4;
+    private Fragment[] fs = new Fragment[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +26,32 @@ public class MainActivity extends AppCompatActivity {
 
         viewPager = findViewById(R.id.viewPager);
         f1 = new F1(); f2 = new F2(); f3 = new F3();
-        fs[0] = f1;fs[1] = f2;fs[2] = f3;
+        f0 = new F0(); f4 = new F4();
+        fs[0] = f0;fs[1] = f1;fs[2] = f2;
+        fs[3] = f3; fs[4] = f4;
         viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
-
+        viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                if (position == 0){
+                    viewPager.setCurrentItem(1);
+                }else if (position == 4){
+                    viewPager.setCurrentItem(3);
+                }
+            }
+        });
+        viewPager.setCurrentItem(1);
     }
 
     public void gotoF1(View view) {
-        viewPager.setCurrentItem(0);
-    }
-    public void gotoF2(View view) {
         viewPager.setCurrentItem(1);
     }
-    public void gotoF3(View view) {
+    public void gotoF2(View view) {
         viewPager.setCurrentItem(2);
+    }
+    public void gotoF3(View view) {
+        viewPager.setCurrentItem(3);
     }
 
     private class MyPagerAdapter extends FragmentStatePagerAdapter {
