31c922da2d26c526a84bc7c99fafd49b8249ae7c
bradchao <brad@brad.tw>
2020-09-05 14:08:06 +0800

    2020090510

--- app/src/main/java/tw/org/tcca/apps/ftest2/F1.java
+++ app/src/main/java/tw/org/tcca/apps/ftest2/F1.java
@@ -2,12 +2,19 @@ package tw.org.tcca.apps.ftest2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Contacts;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ViewFlipper;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class F1 extends Fragment {
     private View mainView;
@@ -28,12 +35,29 @@ public class F1 extends Fragment {
         f3.setOnClickListener(flistener);
         f4.setOnClickListener(flistener);
 
+        timer.schedule(new MyTask(), 1000, 1000);
         return mainView;
     }
 
     private class FListener implements View.OnClickListener {
         @Override
         public void onClick(View view) {
+            viewFlipper.showNext();
+        }
+    }
+    private UIHandler uiHandler = new UIHandler();
+    private Timer timer = new Timer();
+    private class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            uiHandler.sendEmptyMessage(0);
+        }
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
             viewFlipper.showNext();
         }
     }
