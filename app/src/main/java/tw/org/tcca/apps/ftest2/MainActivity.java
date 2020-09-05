4a229b0db334fb7d615adfd5d1ea607ccf102fc8
bradchao <brad@brad.tw>
2020-09-05 11:56:03 +0800

    2020090503

--- app/src/main/java/tw/org/tcca/apps/ftest2/MainActivity.java
+++ app/src/main/java/tw/org/tcca/apps/ftest2/MainActivity.java
@@ -1,14 +1,49 @@
 package tw.org.tcca.apps.ftest2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private F1 f1;
+    private F2 f2;
+    private F3 f3;
+    private Fragment[] fs = new Fragment[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+        f1 = new F1(); f2 = new F2(); f3 = new F3();
+        fs[0] = f1;fs[1] = f2;fs[2] = f3;
+        viewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+
     }
+
+    private class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+        public MyPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fs[position];
+        }
+
+        @Override
+        public int getCount() {
+            return fs.length;
+        }
+    }
+
 }
